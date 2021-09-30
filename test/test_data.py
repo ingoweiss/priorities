7cac220a458ee684fbaabf90685e8f0f893c7ab5
Ingo Weiss <ingoweiss@Ingos-Mac-mini.local>
2021-09-30 15:53:17 -0400

    test multiple dates

--- test/test_data.py
+++ test/test_data.py
@@ -11,3 +11,4 @@ def test_priorities():
 
     assert np.array_equal(priorities.columns, ['Epic', 'Date', 'Priority'])
     assert priorities.set_index(['Date', 'Epic']).loc['2020-05-01', 'Gran Torino'].at['Priority'] == 3
+    assert priorities.set_index(['Date', 'Epic']).loc['2020-06-01', 'Gran Torino'].at['Priority'] == 6
